104042011a25312d0c647481bc268230fd8ad69a
Luisa Laura <luisa.laura.envio@gmail.com>
2025-04-19 15:40:41 -0300

    adiciona o script de concatenação de strings

---
+++ concatenandoDados.py
@@ -0,0 +1,9 @@
+# Vamos receber dois dados diferentes do usuário e  concatená-los em uma única string
+
+info1 = input("Digite o primeiro dado: ")
+info2 = input("Digite o segundo dado: ")
+
+# Concatenando os dados
+
+info = info1 + " " + info2
+print("exibir as informações concatenadas: ", info)
